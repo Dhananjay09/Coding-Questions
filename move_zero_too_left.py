78a1e33b0c2405d80b10f2100a7c70f99c689154
dhananjaysingh <dhananjay.singh@e2enetworks.com>
2023-04-15 09:39:14 +0530

    Given an integer array nums, move all 0's to the start of it while maintaining the relative order of the non-zero elements.

--- move_zero_too_left.py
+++ move_zero_too_left.py
@@ -1,10 +1,9 @@
 class Solution:
-    def moveZeroes(self, nums: List[int]) -> None:
+    def move_zero_to_left(self, nums) -> None:
         left = 0
         i = 0
-        while(i<len(nums)):
-            if nums[i]==0:
-                nums[i],nums[left]=nums[left], nums[i]
-                left+=1
-            i+=1
-        
+        while i < len(nums):
+            if nums[i] == 0:
+                nums[i], nums[left] = nums[left], nums[i]
+                left += 1
+            i += 1
