a9725a76e4f85d10f08b63db3c31bfd1a8ccd9dc
dhananjaysingh <dhananjay.singh@e2enetworks.com>
2023-04-22 07:12:56 +0530

    Given an m x n 2D binary grid grid which represents a map of '1's (land) and '0's (water), return the number of islands.
    An island is surrounded by water and is formed by connecting adjacent lands horizontally or vertically. You may assume all four edges of the grid are all surrounded by water.

--- graph/bfs.py
+++ graph/bfs.py
@@ -35,6 +35,23 @@ class Graph:
                 self.dfs.append(item)
                 self.calculate_dfs(item, visited=visited)
 
+    def numIslands(self, grid):
+        count = 0
+        for i in range(0, len(grid)):
+            for j in range(0, len(grid)):
+                if grid[i][j] == '1':
+                    if i > 0 and grid[i-1][j] == '1':
+                        continue
+                    elif j > 0 and grid[i][j-1] == '1':
+                        continue
+                    elif i < len(grid)-1 and grid[i+1][j] == '1':
+                        continue
+                    elif j < len(grid)-1 and grid[i][j+1] == '1':
+                        continue
+                    else:
+                        count += 1
+        return count+1
+
 
 obj = Graph(graph=graph)
 obj.calculate_bfs('5')
@@ -42,3 +59,4 @@ obj.dfs.append('5')
 obj.calculate_dfs('5', set())
 print(obj.bfs)
 print(obj.dfs)
+
