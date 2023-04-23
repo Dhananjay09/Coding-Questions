48dce3baaf5bbc9b8d5424246d8283cde196523d
dhananjaysingh <dhananjay.singh@e2enetworks.com>
2023-04-23 16:57:21 +0530

    add 2 numbers

---
+++ LinkedList/m-add_to_numbers.py
@@ -0,0 +1,32 @@
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+
+class Solution:
+    def addTwoNumbers(self, l1, l2):
+        breakpoint()
+        temp = l1
+        carry = 0
+        while l1 and l2:
+            if (l1.val+l2.val+carry) > 9:
+                l1.val = l1.val+l2.val+carry - 10
+                carry = 1
+            else:
+                l1.val = l1.val+l2.val+carry
+                carry = 0
+            l1 = l1.next
+            l2 = l2.next
+        return temp
+
+
+l1 = ListNode(2)
+l1.next = ListNode(4)
+l1.next.next = ListNode(3)
+
+l2 = ListNode(5)
+l2.next = ListNode(6)
+l2.next.next = ListNode(4)
+
+Solution().addTwoNumbers(l1, l2)
