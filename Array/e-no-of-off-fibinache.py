71328b07105b838247bbff9bbea630040a551993
dhananjaysingh <dhananjay.singh@e2enetworks.com>
2023-05-13 08:40:34 +0530

    Converted code to functional from Class based

--- Array/e-no-of-off-fibinache.py
+++ Array/e-no-of-off-fibinache.py
@@ -1,15 +1,10 @@
-class Solution:
-    # @param A : integer
-    # @param B : integer
-    # @return an integer
-    def solve(self, A, B):
-        ans = 0
-        while A % 3 != 0:
-            ans += 1
-            A += 1
-        while B % 3 != 0:
-            ans += 1
-            B -= 1
-        ans += ((B - A) * 2) // 3
-        return ans
-
+def solve(A, B):
+    ans = 0
+    while A % 3 != 0:
+        ans += 1
+        A += 1
+    while B % 3 != 0:
+        ans += 1
+        B -= 1
+    ans += ((B - A) * 2) // 3
+    return ans
--- Array/ways_to_climb_stairs.py
+++ Array/ways_to_climb_stairs.py
@@ -1,8 +1,7 @@
-class Solution:
-    def climbStairs(self, n):
-        res = [1, 2, 3]
-        if n <= 3:
-            return res[n-1]
-        for i in range(3, n):
-            res.append(res[i-1]+res[i-2])
+def climbStairs(n):
+    res = [1, 2, 3]
+    if n <= 3:
         return res[n-1]
+    for i in range(3, n):
+        res.append(res[i-1]+res[i-2])
+    return res[n-1]
