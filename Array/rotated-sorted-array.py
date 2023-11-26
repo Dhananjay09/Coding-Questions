46df991741a040bb96c33e0b6ecc0eab404fceed
dhananjay.singh <dhananjay.singh@e2enetworks.com>
2023-11-26 23:19:10 +0530

    rotated sorted array

--- Array/rotated-sorted-array.py
+++ Array/rotated-sorted-array.py
@@ -4,7 +4,7 @@ class Solution:
             return nums[0]
         start = 0
         end = len(nums) - 1
-        while (start <= end):
+        while start <= end:
             mid = (start + end) // 2
             if nums[mid] < nums[mid - 1]:
                 return nums[mid]
